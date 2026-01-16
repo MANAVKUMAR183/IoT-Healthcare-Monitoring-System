e68daa1ad244ef64075d88be7cb6fe370801108c
MANAVKUMAR183 <142963041+MANAVKUMAR183@users.noreply.github.com>
2026-01-16 10:51:57 +0530

    Update firebaseConfig.js

--- ESP32_Code/AI_Model/Web_Dashboard/firebaseConfig.js
+++ ESP32_Code/AI_Model/Web_Dashboard/firebaseConfig.js
@@ -1,11 +1,11 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtn2u2lH6-0yfGRHIhXMnjFKtlgz7JBvI",
   authDomain: "smart-healthcare-iot-5042b.firebaseapp.com",
   databaseURL: "https://smart-healthcare-iot-5042b-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "smart-healthcare-iot-5042b",
-  storageBucket: "smart-healthcare-iot-5042b.firebasestorage.app",
+  storageBucket: "smart-healthcare-iot-5042b.appspot.com",
   messagingSenderId: "789020289820",
   appId: "1:789020289820:web:36dd0a30baeda1b73928d9"
 };
